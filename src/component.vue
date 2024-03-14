20ed91dc3bb6ee88ed22f068807f5339a5c25d26
Lisbeth Berg <lisbeth@elder.no>
2024-03-14 15:28:24 +0100

    fix: replace fontawesome component with iconify fontawesome

--- src/component.vue
+++ src/component.vue
@@ -16,7 +16,7 @@
       </span>
       <span v-if="iconComp || $scopedSlots.icon" class="elder-button__icon">
         <slot name="icon" :icon="iconComp" :state="state">
-          <font-awesome-icon v-bind="iconComp" />
+          <Icon v-bind="iconComp" />
         </slot>
       </span>
     </slot>
@@ -25,9 +25,8 @@
 
 <script>
 import { Options } from '../index'
+import Icon from '@kvass/vue2-icon/src/Legacy'
 import { iconBinding, Capitalize, isPromise } from './utils'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import './icons'
 
 function clickAway(event) {
   if (!this.$el.contains(event.target)) this.resetState()
@@ -194,7 +193,7 @@ export default {
     this.$el.removeEventListener('click', this.clickBinding)
   },
   components: {
-    FontAwesomeIcon,
+    Icon,
   },
 }
 </script>
